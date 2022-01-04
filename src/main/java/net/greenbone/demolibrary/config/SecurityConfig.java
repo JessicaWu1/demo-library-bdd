544848555adae2359f5a835826ec1eac9e65cdb5
jwu <jessica.wu@greenbone.net>
2022-01-04 14:26:18 +0100

    Edited SecurityConfig, application.properties for Keycloak.

--- src/main/java/net/greenbone/demolibrary/config/SecurityConfig.java
+++ src/main/java/net/greenbone/demolibrary/config/SecurityConfig.java
@@ -1,55 +1,71 @@
 package net.greenbone.demolibrary.config;
 
 import lombok.RequiredArgsConstructor;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 import javax.sql.DataSource;
 
-@Configuration
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    //private final DataSource dataSource;
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+            * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll()
-                .anyRequest().permitAll()
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers(
+                        "/login"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and().cors()
                 .and().csrf().disable();
     }
 
-    @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user@example.com").password("password").roles("USER")
-                .and()
-                .withUser("admin@example.com").password("password").roles("ADMIN");
-                /*.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "SELECT email, password FROM applicationUser WHERE email = ?"
-                )
-                .authoritiesByUsernameQuery(
-                        "SELECT u.email, u.role FROM applicationUser u WHERE u.email = ?"
-                );*/
+    //bruahct man das?
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/h2-console/**",
+                "/webjars/**",
+                "/billing/docs/v2/api-docs");
     }
 
-
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
     @Override
     @Bean
@@ -59,8 +75,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
+/*    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .anyRequest().permitAll()
+                .and().csrf().disable();
+    }*/
+
+/*    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user@example.com").password("password").roles("USER")
+                .and()
+                .withUser("admin@example.com").password("password").roles("ADMIN");
+    }*/
+
+
+
+
 }
